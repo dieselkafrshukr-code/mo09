961b4d5121ef7cffcc821ca5f8488d4388df2c68
Admin <admin@restaurant.com>
2026-02-04 18:52:22 +0200

    Change storage bucket to .appspot.com to fix 0% upload hang

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyAh8zgC6yboMgWG56Q3z-GC6VqOWLR3AHY",
     authDomain: "mm10-719bf.firebaseapp.com",
     projectId: "mm10-719bf",
-    storageBucket: "mm10-719bf.firebasestorage.app",
+    storageBucket: "mm10-719bf.appspot.com",
     messagingSenderId: "965633154475",
     appId: "1:965633154475:web:7528fbe7d5a5d79eea86ee",
     measurementId: "G-1J6G73W5MN"
